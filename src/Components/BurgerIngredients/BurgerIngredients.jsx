4ba0e51d13171c9fe554b2a8164ee4f99780d010
Nick <keller108@yandex.ru>
2022-10-30 12:36:58 +0300

    Поправлена верстка компонента с ингридиентами / Стилизован скроллбар

--- src/Components/BurgerIngredients/BurgerIngredients.jsx
+++ src/Components/BurgerIngredients/BurgerIngredients.jsx
@@ -8,25 +8,27 @@ export function BurgerIngredients({
     sauces,
     main
 }) {
-    console.log(buns);
     return (
         <section className={`${ingredientsStyle.container} pt-10`}>
             <h1 className="text text_type_main-large pb-5">
                 Соберите бургер
             </h1>
             <Tabs types={types} />
-            <h2 className="text text_type_main-medium">Булки</h2>
-            <ul className={`${ingredientsStyle.cards} pt-6 pl-4 pb-10 m-0`}>
-                {buns.map((bun) => <Card key={bun._id} {...bun}/>)}
-            </ul>
-            <h2 className="text text_type_main-medium">Соусы</h2>
-            <ul className={`${ingredientsStyle.cards} pt-6 pl-4 pb-10 m-0`}>
-                {sauces.map((sauce) => <Card key={sauce._id} {...sauce}/>)}
-            </ul>
-            <h2 className="text text_type_main-medium">Начинка</h2>
-            <ul className={`${ingredientsStyle.cards} pt-6 pl-4 pb-10 m-0`}>
-                {main.map((main) => <Card key={main._id} {...main}/>)}
-            </ul>
+            <div className={`${ingredientsStyle.ingredients}`}>
+                <h2 className="text text_type_main-medium">Булки</h2>
+                <ul className={`${ingredientsStyle.cards} pt-6 pl-4 pb-10 m-0`}>
+                    {buns.map((bun) => <Card key={bun._id} {...bun}/>)}
+                </ul>
+                <h2 className="text text_type_main-medium">Соусы</h2>
+                <ul className={`${ingredientsStyle.cards} pt-6 pl-4 pb-10 m-0`}>
+                    {sauces.map((sauce) => <Card key={sauce._id} {...sauce}/>)}
+                </ul>
+                <h2 className="text text_type_main-medium">Начинка</h2>
+                <ul className={`${ingredientsStyle.cards} pt-6 pl-4 pb-10 m-0`}>
+                    {main.map((main) => <Card key={main._id} {...main}/>)}
+                </ul>
+            </div>
+
         </section>
     )
 }
