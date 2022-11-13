ca51b0d55e2427d00839b2f59cc65c77dff1ad7c
Nick <keller108@yandex.ru>
2022-11-13 21:59:47 +0300

    Установлены пакета redux / redux thunk / redux toolkit / react-dnd / html5-backend /

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import { App } from './components/App';
 import reportWebVitals from './reportWebVitals';
 import '@ya.praktikum/react-developer-burger-ui-components';
+import { rootReducer } from './services/reducers';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root')
 );
+
+const store = createStore(rootReducer);
+
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
 
