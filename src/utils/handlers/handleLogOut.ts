6617c813f0b423a7dbad5fe40916432c8e0ecee6
Nick <keller108@yandex.ru>
2022-12-05 10:29:47 +0500

    Подправлено удаление токенов из LS

--- src/utils/handlers/handleLogOut.ts
+++ src/utils/handlers/handleLogOut.ts
@@ -4,8 +4,8 @@ export const handleLogOut = () => {
     return logOut()
         .then(res => {
             if (res && res.success) {
-                localStorage.deleteItem('refreshToken', res.refreshToken);
-                localStorage.deleteItem('accessToken', res.accessToken);
+                localStorage.deleteItem('refreshToken');
+                localStorage.deleteItem('accessToken');
             }
         })
         .catch(err => console.log(`Ошибка при разлогинивании – ${err}`))
