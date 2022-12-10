f6fe5d9e6f28fc404c226a006a75bda45e08d7ec
Nick <keller108@yandex.ru>
2022-12-10 11:49:35 +0500

    Добавлены стили страницы 404

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -2,6 +2,8 @@ import style from './NotFound.module.css';
 
 export function NotFound() {
     return (
-        <div>Страница не найдена...</div>
+        <div className={`${style.page} text text_type_main-large`}>
+            Страница не найдена...
+        </div>
     )
 }
