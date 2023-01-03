4bc3bce157f91ae4540e7cd1f35d389c0799b935
Nick <keller108@yandex.ru>
2023-01-03 10:50:11 +0500

    Рутовые типы экшенов переписаны на .ts

--- src/services/actions/index.js
+++
@@ -1,7 +0,0 @@
-export const OPEN_MODAL = 'OPEN_MODAL';
-export const CLOSE_MODAL = 'CLOSE_MODAL';
-
-export const SWITCH_TAB = 'SWITCH_TAB';
-
-export const LOADER_ON = 'LOADER_ON';
-export const LOADER_OFF = 'LOADER_OFF';
