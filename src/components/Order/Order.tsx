d4396768c9e9dd2edc8a9ca68cfd22fcf8934bcb
Nick <keller108@yandex.ru>
2023-01-15 11:20:47 +0500

    Завершена верстка Order.tsx

--- src/components/Order/Order.tsx
+++ src/components/Order/Order.tsx
@@ -13,19 +13,21 @@ type Props = {
 export const Order = ({ number, name, time, ingredients, price }: Props) => {
     return (
         <li className={styles.orderItem}>
-            <div className={styles.orderItemDescription}>
-                <span className='text text_type_main-small'>{number}</span>
+            <div className={`${styles.orderItemDescription} mb-6`}>
+                <span className='text text_type_main-small'>#{number}</span>
                 <span className='text text_type_main-small text_color_inactive'>{time}</span>
             </div>
-            <h2 className='text text_type_main-medium'>{name}</h2>
-            <div>
+            <h2 className='text text_type_main-medium mb-5'>{name}</h2>
+            <div className={styles.description}>
                 <ul className={styles.ingredientsEnumeration}>
                     {ingredients.map(item => <li className={styles.ingredientItem} key={item.name}>
-                        <img src={item.image} alt={item.name} />
+                        <span className={styles.ingredientBackground}><img
+                            className={styles.ingredientImg} src={item.image} alt={item.name} />
+                        </span>
                     </li>)}
                 </ul>
-                <span>
-                    <p>{price}</p>
+                <span className={styles.price}>
+                    <p className='text text_type_digits-default mr-2'>{price}</p>
                     <CurrencyIcon type="primary" />
                 </span>
             </div>
