f556143c7d60a580d45577710b23d5b097ff76f1
Nick <keller108@yandex.ru>
2023-01-15 11:58:04 +0500

    Реилизована верстка Order.tsx

--- src/components/Order/Order.tsx
+++ src/components/Order/Order.tsx
@@ -8,9 +8,10 @@ type Props = {
     time: string;
     ingredients: IIngredientItem[];
     price: number;
+    maxItems: number;
 };
 
-export const Order = ({ number, name, time, ingredients, price }: Props) => {
+export const Order = ({ number, name, time, ingredients, price, maxItems }: Props) => {
     return (
         <li className={styles.orderItem}>
             <div className={`${styles.orderItemDescription} mb-6`}>
@@ -20,7 +21,8 @@ export const Order = ({ number, name, time, ingredients, price }: Props) => {
             <h2 className='text text_type_main-medium mb-5'>{name}</h2>
             <div className={styles.description}>
                 <ul className={styles.ingredientsEnumeration}>
-                    {ingredients.map(item => <li className={styles.ingredientItem} key={item.name}>
+                    {ingredients.map(item => <li key={item.name}
+                        className={styles.ingredientItem} style={{zIndex: maxItems + 1}}>
                         <span className={styles.ingredientBackground}><img
                             className={styles.ingredientImg} src={item.image} alt={item.name} />
                         </span>
--- src/pages/OrderFeed/OrderFeed.tsx
+++ src/pages/OrderFeed/OrderFeed.tsx
@@ -70,6 +70,144 @@ const fakeOrders = [
             }
         ],
         price: 560
+    },
+    {
+        number: 1234,
+        time: 'Сегодня, 16:20 i-GMT+3',
+        name: 'Death Star Starship Main бургер',
+        ingredients: [
+            {
+                calories:  420,
+                carbohydrates: 53,
+                fat : 24,
+                image: "https://code.s3.yandex.net/react/code/bun-02.png",
+                image_large: "https://code.s3.yandex.net/react/code/bun-02-large.png",
+                image_mobile: "https://code.s3.yandex.net/react/code/bun-02-mobile.png",
+                name: "Краторная булка N-200i",
+                price: 1255,
+                proteins: 80,
+                type: "bun",
+                __v: 0,
+                _id: "60d3b41abdacab0026a733c6"
+            },{
+                calories: 643,
+                carbohydrates: 85,
+                fat: 26,
+                image: "https://code.s3.yandex.net/react/code/meat-03.png",
+                image_large: "https://code.s3.yandex.net/react/code/meat-03-large.png",
+                image_mobile: "https://code.s3.yandex.net/react/code/meat-03-mobile.png",
+                name: "Филе Люминесцентного тетраодонтимформа",
+                price: 988,
+                proteins: 44,
+                type: "main",
+                __v: 0,
+                _id: "60d3b41abdacab0026a733c8"
+            }
+        ],
+        price: 480
+    }, {
+        number: 4321,
+        time: 'Сегодня, 16:22 i-GMT+3',
+        name: 'Interstellar бургер',
+        ingredients: [
+            {
+                calories:  420,
+                carbohydrates: 53,
+                fat : 24,
+                image: "https://code.s3.yandex.net/react/code/bun-02.png",
+                image_large: "https://code.s3.yandex.net/react/code/bun-02-large.png",
+                image_mobile: "https://code.s3.yandex.net/react/code/bun-02-mobile.png",
+                name: "Краторная булка N-200i",
+                price: 1255,
+                proteins: 80,
+                type: "bun",
+                __v: 0,
+                _id: "60d3b41abdacab0026a733c6"
+            },{
+                calories: 643,
+                carbohydrates: 85,
+                fat: 26,
+                image: "https://code.s3.yandex.net/react/code/meat-03.png",
+                image_large: "https://code.s3.yandex.net/react/code/meat-03-large.png",
+                image_mobile: "https://code.s3.yandex.net/react/code/meat-03-mobile.png",
+                name: "Филе Люминесцентного тетраодонтимформа",
+                price: 988,
+                proteins: 44,
+                type: "main",
+                __v: 0,
+                _id: "60d3b41abdacab0026a733c8"
+            }
+        ],
+        price: 560
+    },
+    {
+        number: 1234,
+        time: 'Сегодня, 16:20 i-GMT+3',
+        name: 'Death Star Starship Main бургер',
+        ingredients: [
+            {
+                calories:  420,
+                carbohydrates: 53,
+                fat : 24,
+                image: "https://code.s3.yandex.net/react/code/bun-02.png",
+                image_large: "https://code.s3.yandex.net/react/code/bun-02-large.png",
+                image_mobile: "https://code.s3.yandex.net/react/code/bun-02-mobile.png",
+                name: "Краторная булка N-200i",
+                price: 1255,
+                proteins: 80,
+                type: "bun",
+                __v: 0,
+                _id: "60d3b41abdacab0026a733c6"
+            },{
+                calories: 643,
+                carbohydrates: 85,
+                fat: 26,
+                image: "https://code.s3.yandex.net/react/code/meat-03.png",
+                image_large: "https://code.s3.yandex.net/react/code/meat-03-large.png",
+                image_mobile: "https://code.s3.yandex.net/react/code/meat-03-mobile.png",
+                name: "Филе Люминесцентного тетраодонтимформа",
+                price: 988,
+                proteins: 44,
+                type: "main",
+                __v: 0,
+                _id: "60d3b41abdacab0026a733c8"
+            }
+        ],
+        price: 480
+    }, {
+        number: 4321,
+        time: 'Сегодня, 16:22 i-GMT+3',
+        name: 'Interstellar бургер',
+        ingredients: [
+            {
+                calories:  420,
+                carbohydrates: 53,
+                fat : 24,
+                image: "https://code.s3.yandex.net/react/code/bun-02.png",
+                image_large: "https://code.s3.yandex.net/react/code/bun-02-large.png",
+                image_mobile: "https://code.s3.yandex.net/react/code/bun-02-mobile.png",
+                name: "Краторная булка N-200i",
+                price: 1255,
+                proteins: 80,
+                type: "bun",
+                __v: 0,
+                _id: "60d3b41abdacab0026a733c6"
+            },{
+                calories: 643,
+                carbohydrates: 85,
+                fat: 26,
+                image: "https://code.s3.yandex.net/react/code/meat-03.png",
+                image_large: "https://code.s3.yandex.net/react/code/meat-03-large.png",
+                image_mobile: "https://code.s3.yandex.net/react/code/meat-03-mobile.png",
+                name: "Филе Люминесцентного тетраодонтимформа",
+                price: 988,
+                proteins: 44,
+                type: "main",
+                __v: 0,
+                _id: "60d3b41abdacab0026a733c8"
+            }
+        ],
+        price: 560
     }
 ];
 const ready = [12345, 54321, 67890, 9876];
@@ -78,7 +216,7 @@ export const OrderFeed = () => {
     return (
         <main className={styles.orderFeed}>
             <section className={styles.ordersWrapper}>
-                <h1 className="text text_type_main-large mt-5 mb-6">
+                <h1 className="text text_type_main-large mt-8 mb-2">
                     Лента заказов
                 </h1>
                 <ul className={styles.orders}>
@@ -89,6 +227,7 @@ export const OrderFeed = () => {
                         time={item.time}
                         ingredients={item.ingredients}
                         price={item.price}
+                        maxItems={item.ingredients.length}
                     />)}
                 </ul>
             </section>
@@ -96,7 +235,7 @@ export const OrderFeed = () => {
                 <div className={styles.statusWrapper}>
                     <div className={styles.statusCol}>
                         <h2 className='text text_type_main-medium mb-6'>Готовы:</h2>
-                        <ul className={styles.statusList}>
+                        <ul className={`${styles.statusList} ${styles.statusListDone}`}>
                             {ready.map(item => <p className='text text_type_digits-default' key={item}>{item}</p>)}
                         </ul>
                     </div>
