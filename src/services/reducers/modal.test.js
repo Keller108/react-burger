3e0549f60ec77a46b1c582f9a1c4833005e71972
Nick <keller108@yandex.ru>
2023-01-31 20:14:47 +0500

    Редьюсер modalReducer покрыт тестами

--- src/services/reducers/modal.test.js
+++ src/services/reducers/modal.test.js
@@ -1,3 +1,5 @@
+import { ModalType } from "../../shared/types";
+import { CLOSE_MODAL, OPEN_MODAL } from "../constants/modal";
 import { modalReducer } from "./modal";
 
 const initialModalState = {
@@ -8,5 +10,24 @@ const initialModalState = {
 describe('Тестирование Modal Reducer', () => {
     it('Возвращает начальный стейт', () => {
         expect(modalReducer(undefined, {})).toEqual(initialModalState);
+    })
+
+    it('"OPEN_MODAL" отрабатывает корректно', () => {
+        expect(modalReducer(initialModalState, {
+            type: OPEN_MODAL,
+            payload: ModalType.INGREDIENT_VIEW
+        })).toEqual(expect.objectContaining({
+            isActive: true,
+            modalType: ModalType.INGREDIENT_VIEW
+        }))
+    })
+
+    it('"CLOSE_MODAL" отрабатывает корректно', () => {
+        expect(modalReducer(initialModalState, {
+            type: CLOSE_MODAL
+        })).toEqual(expect.objectContaining({
+            isActive: false,
+            modalType: null
+        }))
     })
 });
