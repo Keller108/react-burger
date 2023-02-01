73d7827ce57c95f352696f86e19c69c3b69ed7a6
Nick <keller108@yandex.ru>
2023-02-01 18:39:10 +0500

    deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { compose } from 'redux';
 import './index.css';
@@ -28,9 +28,9 @@ if (window.Cypress !== null) {
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
+            <HashRouter>
                 <App />
-            </BrowserRouter>
+            </HashRouter>
         </Provider>
     </React.StrictMode>
 );
