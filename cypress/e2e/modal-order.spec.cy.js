e9210c9eae65079d58ed68f8956091228ca384ec
Nick <keller108@yandex.ru>
2023-02-01 15:38:28 +0500

    Реализовано покрытие тестами бизнес-логики и поведения приложения

--- cypress/e2e/modal-order.spec.cy.js
+++
@@ -1,8 +0,0 @@
-describe('template spec', () => {
-    beforeEach(function() {
-      cy.visit('http://localhost:3000');
-      cy.intercept("GET", "api/auth/user", { fixture: "user.json" });
-      cy.intercept("POST", "api/orders", { fixture: "order.json" }).as("postOrder");
-
-    });
-})
